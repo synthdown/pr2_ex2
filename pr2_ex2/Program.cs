d4fbffac9d4d0ddd285d062e62abefad2cb6635a
student <soloxpwnz@mail.ru>
2020-09-11 18:15:31 +0300

    Добавьте файлы проекта.

---
+++ pr2_ex2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace pr2_upr2_11_09
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string s = "Microsoft .NET Framework 2.0 Application Development Foundation";
+      string[] sa = s.Split("");
+      Array.Sort(sa);
+      s = string.Join(" ", sa); Console.WriteLine(s);
+    }
+  }
+}
